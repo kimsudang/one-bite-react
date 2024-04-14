7697c74b9d3cc6e44080fa32fbfc7cf47d73fe16
kim suyeon <0ksuyeon0@gmail.com>
2024-04-15 02:43:14 +0900

    5.7강까지 useState, Props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,32 +1,15 @@
-import './App.css'
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
-import Button from './components/Button'
+import './App.css';
+import Bulb from './components/Bulb';
+import Counter from './components/Counter';
 
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-  };
 
   return (
     <>
-    <Header></Header>
-      <h1>안녕 리액트</h1>
-      <Main></Main>
-      <Button {...buttonProps} />
-      <Button text={"카페"} color={"blue"}>
-        <Main></Main>
-      </Button>
-      <Button text={"블로그"}>
-        <div>자식 요소</div>
-      </Button>
-      <Footer></Footer>
+      <Bulb />
+      <Counter/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
