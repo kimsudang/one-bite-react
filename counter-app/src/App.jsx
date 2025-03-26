5bd79f2093a1b361abda6c4f94999d1e5c604fbc
kimsudang <0ksuyeon0@gmail.com>
2025-03-26 23:58:18 +0900

    feat: counter 기능 구현

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import "./App.css";
+import Controller from "./components/Controller";
+import Viewer from "./components/Viewer";
 
 function App() {
-  return <></>;
+  const [count, setCount] = useState(0);
+
+  const onClickEvent = (value) => {
+    setCount(count + value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller onClickEvent={onClickEvent} />
+      </section>
+    </div>
+  );
 }
 
 export default App;
---
+++ counter-app/src/components/Controller.jsx
@@ -0,0 +1,14 @@
+const Controller = ({ onClickEvent }) => {
+  return (
+    <div>
+      <button onClick={() => onClickEvent(-1)}>-1</button>
+      <button onClick={() => onClickEvent(-10)}>-10</button>
+      <button onClick={() => onClickEvent(-100)}>-100</button>
+      <button onClick={() => onClickEvent(+100)}>+100</button>
+      <button onClick={() => onClickEvent(+10)}>+10</button>
+      <button onClick={() => onClickEvent(+1)}>+1</button>
+    </div>
+  );
+};
+
+export default Controller;
